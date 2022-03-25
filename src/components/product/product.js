15e1b5e03a633ee3fbde6bf78c237981f33b0398
ekram666 <ekramahmed2550@gmail.com>
2022-03-26 03:52:30 +0600

    third commit

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Product = (props) => {
     const {name, img, price } = props.product;
 
     return (
-        <div>
-            <img src={img} alt=""></img>
+        <div className='card'>
+            <img className='rounded mx-auto d-block m-3 img-fluid p-4' src={img} alt=""/>
+
             <div>
                 <h6>{name}</h6>
                 <p>${price}</p>
--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../product/product';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -11,15 +12,19 @@ const Shop = () => {
 
     }, []);
     return (
-        <div className='shop-container'>
-            <div className="products-container">
+        <div className='row m-2'>
+            <div className=" col col-lg-9 col-md-9 ">
+                <h4 className='mb-3'>Products</h4>
+               <div className='row row-cols-1 row-cols-md-3 g-3'>
                {
                   products.map(product => <Product key={product.id}
                   product={product}></Product>) 
                }
+               </div>
+
             </div>
-            <div className="cart-container">
-                
+            <div className="cart-container col-3">
+                <h5>Selected Element</h5>
                 </div>
         </div>
     );
